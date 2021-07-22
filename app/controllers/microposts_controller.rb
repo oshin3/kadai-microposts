228f23218ad7b3bedb1222fe4a21822d487ced55
shin <shin3.prg@gmail.com>
2021-07-22 10:46:43 +0000

    フラッシュメッセージの修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,18 +5,18 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'Micropostを投稿しました。'
       redirect_to root_url
     else
       @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      flash.now[:danger] = 'Micropostの投稿に失敗しました。'
       render 'toppages/index'
     end
   end
 
   def destroy
     @micropost.destroy
-    flash[:success] = 'メッセージを削除しました。'
+    flash[:success] = 'Micropostを削除しました。'
     redirect_back(fallback_location: root_path)
   end
 
